1ce7484778525e7207221507e7ffdb473b6d9d3d
Meghrajsinh Solanki <solankimeghraj10@gmail.com>
2020-05-19 01:59:28 +0530

    DTR model with 2 added features: laundry_options and parking_options

--- house_rent.py
+++ house_rent.py
@@ -2,27 +2,38 @@
 
 # importing necessary libraries and modules
 import pandas as pd
-# from numpy import nan
-# from sklearn.tree import DecisionTreeRegressor
+from numpy import nan
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
-# from sklearn.metrics import mean_absolute_error
-# from sklearn import preprocessing
+from sklearn.metrics import mean_absolute_error
+from sklearn import preprocessing
 
 # importing data file for our model
 path = r'D:\iNeuron ML Challenge\Bundle_2\houseRent\housing_train.csv'
 data = pd.read_csv(path)
 y = data.price  # target column
 
-# data[r'laundry_options'] = data[r'laundry_options'].replace('', nan)
+
+# print(data['laundry_options'].describe()) --> this code is to describe
+# laundry_options column. So that we can have the value with most occurences
+# in the column and we can replace nan/ empty values with that value
+data['laundry_options'] = data['laundry_options'].replace(nan, 'w/d in unit')
+# print(data['laundry_options'].isnull().sum()) --> this code is just to verify
+# that we don't have any nan/ empty values in our column after the replacement
+
+
+# print(data['parking_options'].describe()) --> same as laundry_options column
+data['parking_options'] = data['parking_options'].replace(nan, 'off-street parking')
+# print(data['parking_options'].isnull().sum()) --> same as laundry_options column
 
 # preprocessing labels into integer typr which are of string type
-'''
+
 le = preprocessing.LabelEncoder()
 data['type'] = le.fit_transform(data.type)
 data['region'] = le.fit_transform(data.region)
 data['laundry_options'] = le.fit_transform(data.laundry_options)
 data['parking_options'] = le.fit_transform(data.parking_options)
-'''
+
 
 # features used for prediction
 features = ['type', 'region', 'sqfeet', 'beds', 'baths', 'comes_furnished',
@@ -31,18 +42,14 @@ X = data[features]  # features/ columns used for prediction
 
 # splitting data into training data and validation data
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
-print(train_X.shape)
-sum = train_X.isnull().sum()
-print(sum[sum > 0])
 
-'''
 # creating our model using RandomForestRegressor()
 rent_model = DecisionTreeRegressor(random_state=1)  # defining our model
 rent_model.fit(train_X, train_y)  # making model fit for prediction
+
 preds = rent_model.predict(val_X)  # predicting the values
 
 # comparing predicted values and original values
 print("Predictions are: ", rent_model.predict(val_X.head()))
 print("Original values are: ", val_y.head().tolist())
 print("Mean Absolute Error is: ", mean_absolute_error(val_y, preds))
-'''
